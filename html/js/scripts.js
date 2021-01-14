4184c67b4323dc2abfdfe135d0f416c09bf065e2
Eugen M <geka.manager@gmail.com>
2021-01-14 17:22:16 +0200

    HN v5

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,5 +1,11 @@
 $( document ).ready(function() {
 
+    $('.header-page__btn').click(function(){
+		$(this).toggleClass('header-page__btn_opened');
+	});
 
+    $('.header-page__btn').click(function(){
+		$('.header-page__navi').toggleClass('header-page__navi_opened');
+	});
 
 });

7a16099df658a99d310661663f1994fecebd5c9a
Eugen-M <geka.manager@gmail.com>
2021-03-08 21:12:50 +0200

    Catalog

--- catalog/js/scripts-catalog.js
+++ catalog/js/scripts-catalog.js
@@ -1,6 +1,27 @@
 $( document ).ready(function() {
+    // click for search loupe
     $('.search-block__loupe').click(function(){
 		$('.search-block').toggleClass('search-block_opened');
 	});
+
+    // click catalog button more
+    $('.catalog__more').click(function(){
+        if (!($(this).parent(".catalog__box").hasClass("catalog__box_opened"))) {
+            $(this).siblings(".catalog__list_hidden").slideDown();
+            $(this).parents(".catalog__box").addClass("catalog__box_opened");
+        }
+        else {
+            $(this).siblings(".catalog__list_hidden").slideUp();
+            $(this).parents(".catalog__box").removeClass("catalog__box_opened");
+        }
+	});
+
+    // var maxHeight = 0;
+    //
+    // $(".catalog__box").each(function(){
+    //    if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+    // });
+    //
+    // $(".catalog__box").height(maxHeight);
 
 });

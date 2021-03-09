6a12184eb58f32fac2c08ac0cb64824207d832ef
Eugen-M <geka.manager@gmail.com>
2021-03-09 15:02:21 +0200

    Upd About and Catalog

--- about-us/js/scripts.js
+++ about-us/js/scripts.js
@@ -1,34 +1,34 @@
 $( document ).ready(function() {
 
-    $('.header-page__btn').click(function(){
-		$(this).toggleClass('header-page__btn_opened');
+    $('.page-header__btn').click(function(){
+		$(this).toggleClass('page-header__btn_opened');
 	});
 
-    $('.header-page__btn').click(function(){
-		$('.header-page__mob-dropdown_navi').toggleClass('header-page__mob-dropdown_opened');
+    $('.page-header__btn').click(function(){
+		$('.page-header__mob-dropdown_navi').toggleClass('page-header__mob-dropdown_opened');
         $('body').toggleClass('body-hidden');
 	});
 
-    $('.header-page__user-btn').click(function(){
-		$('.header-page__mob-dropdown_active').toggleClass('header-page__mob-dropdown_opened');
-        $('.header-page__btn').toggleClass('header-page__btn_hidden');
-        $('.header-page__user-btn').toggleClass('header-page__user-btn_opened');
+    $('.page-header__user-btn').click(function(){
+		$('.page-header__mob-dropdown_active').toggleClass('page-header__mob-dropdown_opened');
+        $('.page-header__btn').toggleClass('page-header__btn_hidden');
+        $('.page-header__user-btn').toggleClass('page-header__user-btn_opened');
         $('body').toggleClass('body-hidden');
 	});
 
-    $('.header-page__user-login').click(function(){
-		$('.header-page__mob-dropdown_register').toggleClass('header-page__mob-dropdown_opened');
-        $('.header-page__btn').toggleClass('header-page__btn_hidden');
-        $('.header-page__user-login').toggleClass('header-page__user-login_opened');
+    $('.page-header__user-login').click(function(){
+		$('.page-header__mob-dropdown_register').toggleClass('page-header__mob-dropdown_opened');
+        $('.page-header__btn').toggleClass('page-header__btn_hidden');
+        $('.page-header__user-login').toggleClass('page-header__user-login_opened');
         $('body').toggleClass('body-hidden');
 	});
 
-    $('.header-page__search-loupe').click(function(){
-		$('.header-page__search').toggleClass('header-page__search_opened');
+    $('.page-header__search-loupe').click(function(){
+		$('.page-header__search').toggleClass('page-header__search_opened');
 	});
 
-    $('.header-page__search-loupe').click(function(){
-		$('.header-page').toggleClass('header-page_active-search');
+    $('.page-header__search-loupe').click(function(){
+		$('.page-header').toggleClass('page-header_active-search');
 	});
 });
 

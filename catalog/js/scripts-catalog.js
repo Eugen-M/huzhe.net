2a2d7ce807f4bf8ce2bb2d0b918a8841e2c24323
Eugen-M <geka.manager@gmail.com>
2021-03-09 14:01:26 +0200

    Catalog Upd

--- catalog/js/scripts-catalog.js
+++ catalog/js/scripts-catalog.js
@@ -1,9 +1,4 @@
 $( document ).ready(function() {
-    // click for search loupe
-    $('.search-block__loupe').click(function(){
-		$('.search-block').toggleClass('search-block_opened');
-	});
-
     // click catalog button more
     $('.catalog__more').click(function(){
         if (!($(this).parent(".catalog__box").hasClass("catalog__box_opened"))) {
@@ -15,13 +10,4 @@ $( document ).ready(function() {
             $(this).parents(".catalog__box").removeClass("catalog__box_opened");
         }
 	});
-
-    // var maxHeight = 0;
-    //
-    // $(".catalog__box").each(function(){
-    //    if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
-    // });
-    //
-    // $(".catalog__box").height(maxHeight);
-
 });
